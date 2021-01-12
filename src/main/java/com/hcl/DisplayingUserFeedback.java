34a661a19764f26f7a088757334c09c2288db28d
chefvivica <vivicacorn@gmail.com>
2021-01-11 23:26:01 -0500

    updated

--- src/main/java/com/hcl/DisplayingUserFeedback.java
+++ src/main/java/com/hcl/DisplayingUserFeedback.java
@@ -7,10 +7,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class SpringmvcupdateusersApplication {
+public class DisplayingUserFeedback {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringmvcupdateusersApplication.class, args);
+		SpringApplication.run(DisplayingUserFeedback.class, args);
 	}
 	
 	@Bean
--- src/main/java/com/hcl/controller/FeedbackController.java
+++ src/main/java/com/hcl/controller/FeedbackController.java
@@ -1,35 +1,20 @@
 package com.hcl.controller;
-
-import java.net.URI;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import com.hcl.entity.Feedback;
 import com.hcl.service.ServiceInterface;
 
-import Exception.ResourceNotFoundException;
+
 
 @RestController
 @RequestMapping
